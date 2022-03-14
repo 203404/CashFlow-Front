a2b86020c7df15f09dc857324a6c4de0664a54c9
Ale Mota <203404@ids.upchiapas.edu.mx>
2022-03-14 13:46:37 -0600

    axios post

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,16 +1,52 @@
 import React, { Component } from "react";
+import axios from "axios";
+
 export default class Login extends Component {
+
+    state={
+        form:{
+            'email':'',
+            'password':''
+        },
+        error:false,
+        errorMessage:''
+    }
+
+    manejadorSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    manejadorChange = async e=>{
+        await this.setState({
+            form:{
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            
+            }
+        })
+        console.log(this.state.form);
+    }
+
+    manejadorBoton=()=>{
+        console.log('enviado')
+        let url;
+        axios.post(url,this.state.form)
+        .then(res=>{
+            console.log(res);
+        })
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.manejadorSubmit}> 
                 <h3>Sign In</h3>
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" className="form-control" name='email' placeholder="Enter email" onChange={this.manejadorChange}/>
                 </div>
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" className="form-control" name='password' placeholder="Enter password" onChange={this.manejadorChange} />
                 </div>
                 <div className="form-group">
                     <div className="custom-control custom-checkbox">
@@ -18,7 +54,7 @@ export default class Login extends Component {
                         <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
                     </div>
                 </div>
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <button type="submit" className="btn btn-primary btn-block" onClick={this.manejadorBoton}>Submit</button>
                 <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
                 </p>
@@ -26,3 +62,4 @@ export default class Login extends Component {
         );
     }
 }
+
