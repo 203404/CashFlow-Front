bebc748583782b970df54981c792088bfe21f01e
203423 <203423@ids.upchiapas.edu.mx>
2022-03-18 12:03:30 -0600

    Creación de menú

---
+++ src/components/mainMenu.component.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Sidebar extends React.Component {
+    render() {
+        return (
+            <div className="any">
+                <table class="table table-bordered">
+                    <tbody>
+                        <tr>
+                            <td>Categoria</td>
+                        </tr>
+                        <tr>
+                            <td>Flujo</td>
+                        </tr>
+                        <tr>
+                            <td>Hola</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+export default Sidebar;
