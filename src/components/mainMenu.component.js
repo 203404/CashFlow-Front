2f12bc2b890722d515a281fdf34fdbac2280c7b9
203423 <203423@ids.upchiapas.edu.mx>
2022-03-18 14:49:23 -0600

    Actualización mainMenu

--- src/components/mainMenu.component.js
+++ src/components/mainMenu.component.js
@@ -5,6 +5,8 @@ class MainMenu extends React.Component {
 
     render() {
         return (
+            <div className="navbar navbar-expand-lg">
+            <div className="container">
             <div >
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
@@ -17,6 +19,8 @@ class MainMenu extends React.Component {
                         <Link className="nav-link" to={"/registros"}>Indicadores</Link>
                     </li>
                 </ul>
+            </div>
+            </div>
             </div>
         );
     }
